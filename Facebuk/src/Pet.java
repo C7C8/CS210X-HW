392d6a0aebe35d02d9caa8bd1dd2dc9ed1cc9aa6
Mike <mjkrebs@wpi.edu>
2016-10-30 19:07:09 -0400

    A lot of mundane code

--- Facebuk/src/Pet.java
+++ Facebuk/src/Pet.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
 
 public class Pet {
+	ArrayList friends = new ArrayList(); // can have both Person and Pet objects
+	ArrayList<Person> owners = new ArrayList<Person>();
+	ArrayList<Moment> moments = new ArrayList<Moment>();
 
+	private String name;
+	private Image picture;
+
+	public Pet(){};
+
+	public Pet(String n, Image i){
+		name = n;
+		picture = i;
+	}
+
+	public String getName(){
+		return name;
+	}
+	public Image getImage(){
+		return picture;
+	}
+	
+	public void setFriends(ArrayList<Person> f){
+		for(Person x : f){
+		 friends.add(x);
+		}
+	}
+	
+	public void setOwners(ArrayList<Person> f){
+		for(Person x : f){
+		 owners.add(x);
+		}
+	}
+
+	public void removeFriend(Person p){
+		friends.remove(p);
+	}
 }
--- Facebuk/src/Possession.java
+++ Facebuk/src/Possession.java
@@ -1,4 +1,26 @@
 
 public class Possession {
 
+	private Person owner;
+	private String name;
+	private Image picture;
+	private Float price;
+	
+	public Possession(String n, Image i, Float money){
+		name = n;
+		picture = i;
+		price = money;
+	}
+	
+	public Float getPrice(){
+		return price;
+	}
+	
+	public Image getImage(){
+		return picture;
+	}
+	
+	public void setOwner(Person p){
+		owner = p;
+	}
 }
