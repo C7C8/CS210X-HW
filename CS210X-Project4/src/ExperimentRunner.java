cd916951eb940d6e8c5a6de624d405f65fe48cc8
Sourec <crmyers@wpi.edu>
2016-12-05 10:00:49 -0500

    Add new batch of 10000Tx1000N data, automate data collection
    -New data has been added in a series of 10 different CSV files. Each
     data point is averages over 10000 trials with a maximum collection
     size of 1000.
    -Data collection has now been automated. The program will now conduct
     all possible trials and collections automatically, writing the results
     of each to the appropriately named file.

--- CS210X-Project4/src/ExperimentRunner.java
+++ CS210X-Project4/src/ExperimentRunner.java
@@ -9,69 +9,52 @@ import com.cs210x.*;
   * Class to deduce the identity of mystery data structures.
   */
 public class ExperimentRunner {
-	enum MODES {ADD, REMOVE, SEARCH};
 	
 	public static void main (String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		if (args.length != 3){
-			System.out.println("Not enough arguments!");
-			System.out.println("Usage: java ExperimentRunner [teamID] [algorithmID] [testID]");
-			System.out.println("Test ID options are \"add\", \"remove\", and \"search\"");
-			throw new InvalidParameterException();
-		}
+		final int teamID = 1036;
+		final int TRIALS = 10000;	//How many trials to run
+		final int MAX_N = 1000;		//The maximum size of a collection
+		
+		final int ADD = 0;
+		final int REMOVE = 1;
 
-		final int TRIALS = 1000;	//How many trials to run
-		final int MAX_N = 1000;	//The maximum size of a collection
-		final int teamID = Integer.parseInt(args[0]); 
-		final int algoID = Integer.parseInt(args[1]);
-		MODES mode = MODES.ADD;
-	
-		switch (args[2]){
-		case "add":
-			mode = MODES.ADD;
-			break;
-		case "remove":
-			mode = MODES.REMOVE;
-			break;
-		default:
-			System.out.println("Bad mode specified, switching to ADD");
-			mode = MODES.ADD;
-		}
-		
-		
-		@SuppressWarnings("unchecked") 
-		Collection210X<Integer> dataStructure = MysteryDataStructure.getMysteryDataStructure(teamID, algoID, new Integer(0));
-		
-		Random rand = new Random(0);
-		long[] times = new long[MAX_N];
-		
-		
-		for (int trial = 0; trial < TRIALS; trial++){
-			if (mode == MODES.REMOVE){
-				//Populate collection with random values
+		for (int algoID = 0; algoID < 5; algoID++){
+			for (int mode = ADD; mode <= REMOVE; mode++){
+
+				@SuppressWarnings("unchecked") 
+				Collection210X<Integer> dataStructure = MysteryDataStructure.getMysteryDataStructure(teamID, algoID, new Integer(0));
+
+				Random rand = new Random(0);
+				long[] times = new long[MAX_N];
+
+
+				for (int trial = 0; trial < TRIALS; trial++){
+					if (mode == REMOVE){
+						//Populate collection with random values
+						for (int n = 0; n < MAX_N; n++)
+							dataStructure.add(rand.nextInt());
+					}
+
+					for (int n = 0; n < MAX_N; n++){
+						final long startTime = CPUClock.getNumTicks();
+
+						if (mode == ADD)
+							dataStructure.add(rand.nextInt());
+						else if (mode == REMOVE)
+							dataStructure.remove(rand.nextInt() % dataStructure.size());
+						
+						times[n] += CPUClock.getNumTicks() - startTime;
+					}
+					dataStructure.clear();
+				}
+
+				//Now print averages
+				PrintWriter writer = new PrintWriter(String.format("%d-%s.csv", algoID, mode == ADD ? "ADD" : "REMOVE"), "UTF-8");
 				for (int n = 0; n < MAX_N; n++)
-					dataStructure.add(rand.nextInt());
+					writer.printf("%d, %d\n", n, times[n] / TRIALS);
+				writer.close();
+				System.out.printf("Finished writing '%s'!\n", String.format("%d-%s.csv", algoID, mode == ADD ? "ADD" : "REMOVE"));
 			}
-			
-			for (int n = 0; n < MAX_N; n++){
-				final long startTime = CPUClock.getNumTicks();
-				
-				if (mode == MODES.ADD)
-					dataStructure.add(rand.nextInt());
-				else if (mode == MODES.REMOVE)
-					dataStructure.remove(rand.nextInt() % dataStructure.size());
-				//No search, because we don't need searches... right?
-				
-				times[n] += CPUClock.getNumTicks() - startTime;
-			}
-			dataStructure.clear();
 		}
-		
-		//Now print averages
-		PrintWriter writer = new PrintWriter(String.format("%d-%s.csv", algoID, args[2]), "UTF-8");
-		for (int n = 0; n < MAX_N; n++)
-			writer.printf("%d, %d\n", n, times[n] / TRIALS);
-		writer.close();
-		System.out.printf("Finished writing '%s'!\n", String.format("%d-%s.csv", algoID, args[2]));
-			
 	}
 }
