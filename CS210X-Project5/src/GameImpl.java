C7C8/CS210X-HW

commit b120e0d2bd3da402fad68d380b26a5fceeb9cabd

Add brick removal upon ball contact

CS210X-Project5/src/GameImpl.java
@@ -50,7 +50,7 @@ public class GameImpl extends Pane implements Game {
 			for (int iY = 0; iY < BRICK_ROWS; iY++){
 				Brick x = new Brick(iX * (Brick.BRICK_WIDTH + BRICK_GAP) + BRICK_X_OFFSET,
 						iY * (Brick.BRICK_HEIGHT + BRICK_GAP) + BRICK_Y_OFFSET);
-				getChildren().add(x.getRectangle()); 
+				getChildren().add(x.getRectangle());
 				bricks[iX][iY] = x;
 			}
 		}
@@ -119,20 +119,30 @@ public class GameImpl extends Pane implements Game {
 	 * @return the current game state
 	 */
 	public GameState runOneTimestep (long deltaNanoTime) {
-		if(ball.getY()+ball.BALL_RADIUS>=HEIGHT){
+		//Remove bricks upon ball impact
+		for (int iX = 0; iX < BRICK_COLUMNS; iX++){
+			for (int iY = 0; iY < BRICK_ROWS; iY++){
+				if (bricks[iX][iY] != null && ball.interesectRect(bricks[iX][iY].getRectangle())){
+					getChildren().remove(bricks[iX][iY].getRectangle());
+					bricks[iX][iY] = null;
+				}
+			}
+		}
+
+		if(ball.getY() + Ball.BALL_RADIUS >= HEIGHT){
 			bottomTouches++;
 		}
 		ball.updatePosition(deltaNanoTime, paddle);
 		
 				
 		// below are the tests to see how the game is going
-		if(LOSE_BOTTOM_COLLISIONS<=bottomTouches){
+		if(LOSE_BOTTOM_COLLISIONS <= bottomTouches){
 			return GameState.LOST;
 		}
 		boolean won = true;
 		for (int iX = 0; iX < BRICK_COLUMNS; iX++){
 			for (int iY = 0; iY < BRICK_ROWS; iY++){
-				if(bricks[iX][iY]!=null){
+				if(bricks[iX][iY] != null){
 					won = false;
 				}
 			}

CS210X-Project5/src/Ball.java
@@ -9,7 +9,7 @@ import javafx.scene.shape.Rectangle;
 public class Ball {
 	// Constants
 	public static final int BALL_RADIUS 	= 8;
-	public static final double INITIAL_VEL 	= 2e-7;
+	public static final double INITIAL_VEL 	= 3e-7;
 
 	// Instance variables
 	// (x,y) is the position of the center of the ball.

commit 7f062890b1a74e6774cf2101451f1e87c965a9f4

Fix bug in continuous losses after first loss
Turns out, the bug wasn't introduced in b120e0d like previously thought.
The game just wasn't programmed to reset the bottom touches count upon
restart.

CS210X-Project5/src/GameImpl.java
@@ -40,11 +40,12 @@ public class GameImpl extends Pane implements Game {
 
 	private void restartGame (GameState state) {
 		getChildren().clear();  // remove all components from the game
+		bottomTouches = 0;
 
 		// Create and add ball
 		ball = new Ball();
 		getChildren().add(ball.getCircle());  // Add the ball to the game board
-		
+
 		// Create and add bricks
 		for (int iX = 0; iX < BRICK_COLUMNS; iX++){
 			for (int iY = 0; iY < BRICK_ROWS; iY++){
@@ -133,16 +134,15 @@ public class GameImpl extends Pane implements Game {
 			}
 		}
 
-		if(ball.getY() + Ball.BALL_RADIUS >= HEIGHT){
+		if(ball.getY() + Ball.BALL_RADIUS >= HEIGHT)
 			bottomTouches++;
-		}
+
 		ball.updatePosition(deltaNanoTime, paddle);
 				
 		// below are the tests to see how the game is going
-		if(LOSE_BOTTOM_COLLISIONS <= bottomTouches){
+		if(LOSE_BOTTOM_COLLISIONS <= bottomTouches)
 			return GameState.LOST;
-		}
-		
+
 		boolean won = true;
 		double newSpeed = Ball.MAX_VEL;
 		for (int iX = 0; iX < BRICK_COLUMNS; iX++){
@@ -153,6 +153,7 @@ public class GameImpl extends Pane implements Game {
 				}
 			}
 		}
+
 		ball.setSpeed(newSpeed + Ball.INITIAL_VEL); //Ball will increase speed as number of bricks decreases
 		if (won)
 			return GameState.WON;